3f7808b71b2d7b1af0051018748c3f7c1b45ca83
Sukoreno Mukti <sukorenomw@gmail.com>
2015-05-20 10:20:51 +0700

    change phone type from integer to string, finished add user module, added validation for adding user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,22 @@ class UsersController < ApplicationController
   	@user = User.new
   end
 
+  def create
+  	@user = User.new(params_user)
+  	if @user.save
+  		flash[:notice] = "User successfully added!"
+  		redirect_to action: "index"
+  	else
+  		flash[:error] = "data not valid"
+        render 'new'
+  	end
+  end
+
   def edit
   end
+
+   private
+    def params_user
+        params.require(:user).permit(:name, :email, :phone, :address)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+	validates :name, presence: true, length: {minimum: 3}
+	validates_format_of :email, presence: true, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+	validates :phone, presence: true, length: {minimum: 6}, numericality: { only_integer: true }
+	validates :address, presence: true, length: {minimum: 3}
 end
